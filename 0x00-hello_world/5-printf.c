57035fea3b6fc1b2c22f9149775c5d3393d4add7
ismailbenrkiya <ismailbenrkya@gmail.com>
2023-03-16 15:30:22 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Print out a string to stdout
+ * Return : 0 (Succes)
+ */
+int main(void)
+	{
+		Print("with proper grammar, but the outcome is a piece of art,\n");
+		Return (0);
+	}
