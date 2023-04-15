55e20da30f955b6608d993550f2b1bdb5ee9f080
ismailbenrkiya <ismailbenrkya@gmail.com>
2023-04-14 19:05:14 -0700

    0-malloc_checked.c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -2,35 +2,17 @@
 #include <stdlib.h>
 
 /**
- * create_array - create array of size size and assign char c
- * @size: size of array
- * @c: char to assign
- * Description: creat array of size size and assign char c
- * Return: pointer to array, NULL if fail
+ * malloc_checked - Allocates memory using malloc.
+ * @b: The number of bytes to be allocated.
  *
+ * Return: A pointer to the allocated memory.
  */
-
-char *create_array(unsigned int size, char c)
+void *malloc_checked(unsigned int b)
 {
-	char *str;
-	unsigned int i;
+	void *mem = malloc(b);
 
-	if (size == 0)
-	{
-		return (NULL);
-	}
+	if (mem == NULL)
+		exit(98);
 
-	str = (char *) calloc(size, sizeof(char));
-
-	if (str == NULL)
-	{
-		return (NULL);
-	}
-
-	for (i = 0; i < size; i++)
-	{
-		str[i] = c;
-	}
-
-	return (str);
+	return (mem);
 }
