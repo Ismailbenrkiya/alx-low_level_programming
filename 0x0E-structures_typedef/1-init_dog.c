5a49416f057da545e1e5906b031d75b756b16b44
ismailbenrkiya <ismailbenrkya@gmail.com>
2023-04-17 19:52:00 -0700

    dog.h

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -8,7 +8,7 @@
  * @age: age to initialize
  * @owner: owner to initialize
  */
-void init_dog(struct dog *d, char *name, float age, char *owner)
+;void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	struct dog new_dog;
 
--- 0x0E-structures_typedef/1-main.c
+++ 0x0E-structures_typedef/1-main.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 #include "dog.h"
+
 /**
  * main - check the code
  *
  * Return: Always 0.
  */
-int main(void)
+;int main(void)
 {
 	struct dog my_dog;
 
